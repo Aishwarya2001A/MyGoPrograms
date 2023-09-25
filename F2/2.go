78532a91dc646622cb826e90ebd0decb5dc76eff
Aishwarya2001A <aaishwar@progress.com>
2023-09-25 08:21:18 +0530

    p2

--- F2/2.go
+++ F2/2.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	const pi float64 = 3.14
 	var i int
 	var f float64
 	var b bool
 	var s string
+	fmt.Println(pi)
 	fmt.Printf("%T %T %T %T\n", i, f, b, s)              // Prints type of the variable
 	fmt.Printf("%v   %v      %v  %q     \n", i, f, b, s) //prints initial value of the variable
 }
