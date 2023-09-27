fc4c66f19abff5244975167e5213379c5ea4caa9
Aishwarya2001A <aaishwar@progress.com>
2023-09-27 10:32:25 +0530

    p34

---
+++ F34/34.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type tank interface {
+	Tarea() float64
+	Volume() float64
+}
+
+type myvalue struct {
+	radius float64
+	height float64
+}
+
+func (m myvalue) Tarea() float64 {
+
+	return 2*m.radius*m.height +
+		2*3.14*m.radius*m.radius
+}
+
+func (m myvalue) Volume() float64 {
+
+	return 3.14 * m.radius * m.radius * m.height
+}
+
+func myfun(a interface{}) {
+
+	// Using type switch
+	switch a.(type) {
+
+	case int:
+		fmt.Println("Type: int, Value:", a.(int))
+	case string:
+		fmt.Println("\nType: string, Value: ", a.(string))
+	case float64:
+		fmt.Println("\nType: float64, Value: ", a.(float64))
+	default:
+		fmt.Println("\nType not found")
+	}
+}
+
+func main() {
+
+	var t tank
+	t = myvalue{10.6, 14.99}
+	fmt.Println("Area of tank :", t.Tarea())
+	fmt.Println("Volume of tank:", t.Volume())
+	myfun("Goprogramming")
+	myfun(67.9)
+	myfun(true)
+}
