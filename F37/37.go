814c38dff86d17ab1fd3749f3d73758a21ff95d8
Aishwarya2001A <aaishwar@progress.com>
2023-09-27 11:28:31 +0530

    p37

--- F37/37.go
+++ F37/37.go
@@ -29,4 +29,17 @@ func main() {
 	newBlean := strconv.FormatBool(blean)
 	fmt.Printf("After conversion:\n Type: %T\n Value: %v\n", newBlean, newBlean)
 
+	var str1 string
+
+	str1 = "40.98"
+
+	fmt.Println(strconv.ParseFloat(str1, 32))
+	fmt.Println(strconv.ParseFloat(str1, 64))
+	fmt.Println()
+
+	str2 := "-104.23101"
+	fmt.Println(strconv.ParseFloat(str2, 32))
+	fmt.Println(strconv.ParseFloat(str2, 64))
+	fmt.Println()
+
 }
