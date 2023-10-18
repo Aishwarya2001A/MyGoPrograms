e705d6f40e02df7f76bbe7dd65336e6f44b8701b
Aishwarya2001A <aaishwar@progress.com>
2023-10-18 10:26:48 +0530

    Assignment

--- assignment/main.go
+++ assignment/main.go
@@ -216,7 +216,7 @@ func main() {
 	fmt.Println("Comp7S20211122 ProvidedName:", data.Comp7S20211122.Settings.Index.ProvidedName)
 	fmt.Println("Comp7S20211122 CreationDate:", data.Comp7S20211122.Settings.Index.CreationDate)
 
-	fmt.Println("------------------------------------------------------------------")
+	fmt.Println("-------------------------------------------------------------------")
 
 	fmt.Println("Comp7S20211123 Refresh Interval:", data.Comp7S20211123.Settings.Index.RefreshInterval)
 	fmt.Println("Comp7S20211123 Number of Shards:", data.Comp7S20211123.Settings.Index.NumberOfShards)
